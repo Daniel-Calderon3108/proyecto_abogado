a5dee2770fedf0161cb0bdc35e88408345bb8a60
Daniel Calderon <danielcalderon105@gmail.com>
2024-10-15 18:53:09 -0500

    Info Cliente
    Se crea el componente el cual permite mostrar info cliente

--- angular/src/app/view-customer/view-customer.component.ts
+++ angular/src/app/view-customer/view-customer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CustomersService } from '../services/customers.service';
+import { DataService } from '../services/shared/data.service';
 
 @Component({
   selector: 'app-view-customer',
@@ -10,16 +11,36 @@ import { CustomersService } from '../services/customers.service';
 export class ViewCustomerComponent implements OnInit {
 
   idClient : string = "";
+  data: any = [];
+  isDarkMode: boolean = localStorage.getItem("darkMode") === "true";
 
-  constructor(private activatedRoute : ActivatedRoute, private customerService : CustomersService) { }
+  constructor(private activatedRoute : ActivatedRoute, private customerService : CustomersService,
+    private dataService : DataService) { }
 
   ngOnInit(): void {
     this.idClient = this.activatedRoute.snapshot.paramMap.get("id") || "";
-    this.customer(parseInt(this.idClient));
+    this.customer();
+    this.heightInfo();
+    
+    this.dataService.currentDarKMode.subscribe( value => { this.isDarkMode = value; });
   }
 
-  customer(id : number) {
+  customer() {
+    this.customerService.getCustomerByID(parseInt(this.idClient))
+    .subscribe(
+      rs => {
+        this.data = rs;
+      },
+      err => console.log(err)
+    )
+  }
 
+  heightInfo() {
+    let height: number = document.documentElement.clientHeight;
+
+    const operationsElement = document.getElementById("info");
+
+    if (operationsElement) operationsElement.style.maxHeight = `${height - 140}px`;
   }
 
 }
